1e0942cef80c6e1d823408f9fe0784be3909208e
Serov Dmitriy <serov>
2022-08-08 18:06:42 +0500

    Initial Commit

---
+++ Employder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employder
+//
+//  Created by Serov Dmitry on 08.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
